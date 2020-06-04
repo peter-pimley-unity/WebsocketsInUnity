2741bb3358de47b6ea12455141490f2acc15d997
Peter Pimley <peter.pimley@unity3d.com>
2020-06-04 14:43:30 +0100

    Major improvement to UI, etc.

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -11,64 +11,129 @@ public class Test : MonoBehaviour
 {
 
 	private CancellationToken m_cancel;
-	private ClientWebSocket m_socket;
 
-	private Task<string> m_task;
-	private string m_result;
-
-	public bool IsReady => m_socket != null && m_task == null;
+	private ClientWebSocket m_socket = null;
 
 
-	[SerializeField]
-	private Text m_sentText;
-	[SerializeField]
-	private Text m_receivedText;
+	public event System.Action<string> MessageSent, MessageReceived;
 
-	// Start is called before the first frame update
-	void Start()
+
+	// Replies are placed in here by the worker thread,
+	// and collected and processed by the main thread.
+	private List<string> m_replies = new List<string>();
+
+
+	public bool IsWaitingForReply { get; private set; }
+
+	public bool IsReadyToSend => WebSocketState == WebSocketState.Open && !IsWaitingForReply;
+
+	private bool m_tls = true;
+	public bool IsUsingTls {
+		get {
+			return m_tls;
+		}
+		set {
+			var state = WebSocketState;
+			if (state != WebSocketState.None)
+				throw new System.Exception(
+					$"Can only set when state is None.  Currently {state}.");
+			m_tls = value;
+		}
+	}
+
+
+	// We poll the state on the main thread every frame, looking for changes.
+	private WebSocketState m_lastState = WebSocketState.None;
+	public WebSocketState WebSocketState => m_socket != null ? m_socket.State : WebSocketState.None;
+
+	public System.Action WebSocketStateChanged; // Sends the old and the new states.
+
+	private void Awake()
 	{
-		m_cancel = new CancellationTokenSource().Token; // Is this needed?
-		Task<ClientWebSocket> create = CreateAndConnect("wss://echo.websocket.org", m_cancel);
-		create.ContinueWith(task => m_socket = task.Result);
+		m_socket = new ClientWebSocket();
+	}
+
+	private void Update()
+	{
+		PollSocket();
+		NotifyReplies();
+	}
+
+	private void PollSocket ()
+	{
+		WebSocketState newState = m_socket != null ? m_socket.State : WebSocketState.None;
+		bool change = newState != m_lastState;
+		m_lastState = newState;
+		if (change)
+			if (WebSocketStateChanged != null)
+				WebSocketStateChanged();
+	}
+
+	private void NotifyReplies()
+	{
+		lock (m_replies)
+		{
+			if (m_replies.Count == 0)
+				return;
+			foreach (string s in m_replies)
+				if (MessageReceived != null)
+					MessageReceived(s);
+			m_replies.Clear();
+		}
+
 	}
 
 
 
-	private async Task<ClientWebSocket> CreateAndConnect (string uriStr, CancellationToken cancel)
+	public void StartConnecting()
 	{
-		ClientWebSocket client = new ClientWebSocket();
-		var connect = client.ConnectAsync(new System.Uri(uriStr), cancel);
-		await connect;
-		return client;
-	}
-
-
-	public void DoWebsocketSend ()
-	{
-		if (!IsReady)
+		Debug.Log("Connecting.");
+		var state = WebSocketState;
+		bool closed = state == WebSocketState.Closed || state == WebSocketState.None;
+		if (!closed)
+		{
+			Debug.LogWarning($"StartConnecting called when not fully closed (state was {state}.");
 			return;
+		}
+
+		string protocol = IsUsingTls ? "wss" : "ws";
+		string url = $"{protocol}://echo.websocket.org";
+		Debug.Log($"Connecting to {url}");
+		m_socket = new ClientWebSocket();
+		m_socket.ConnectAsync(new System.Uri(url), m_cancel);
+	}
+
+
+	public void DoWebsocketSend()
+	{
+		Debug.Log("Sending");
+		bool ready = WebSocketState == WebSocketState.Open && !IsWaitingForReply;
+		if (!ready)
+		{
+			Debug.LogWarning($"Ignoring request to send when state is {WebSocketState} and IsWaitingForReply={IsWaitingForReply}.");
+			return;
+		}
 
 		string s = $"Hello: {System.Guid.NewGuid().ToString()}";
-		m_task = SendAndReceive(m_socket, s, m_cancel);
-		m_sentText.text = $"Sent: {s}";
-		m_task.ContinueWith(delegate (Task<string> t)
+
+		if (MessageSent != null)
+			MessageSent(s);
+
+		IsWaitingForReply = true;
+		Task<string> sendReceive = SendAndReceive(m_socket, s, m_cancel);
+
+		sendReceive.ContinueWith(delegate (Task<string> t)
 		{
-			m_result = t.Result;
-			m_task = null;
+			IsWaitingForReply = false;
+			string reply = t.Result;
+			lock (m_replies)
+				m_replies.Add(reply);
 		});
 	}
 
 
-	void Update()
-	{
-		if (! string.IsNullOrEmpty(m_result))
-		{
-			m_receivedText.text = $"Received: {m_result}";
-			m_result = null;
-		}
-	}
 
-	private async Task<string> SendAndReceive (ClientWebSocket socket, string mesg, CancellationToken cancel)
+	private static async Task<string> SendAndReceive (ClientWebSocket socket, string mesg, CancellationToken cancel)
 	{
 		System.Text.Encoding utf8 = System.Text.Encoding.UTF8;
 		byte[] bytes = utf8.GetBytes(mesg);
@@ -85,4 +150,15 @@ public class Test : MonoBehaviour
 		return s;
 	}
 	
+
+	public void StartDisconnecting ()
+	{
+		if (!IsReadyToSend)
+		{
+			Debug.LogWarning("Asked to disconnect while busy");
+			return;
+		}
+		m_socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Bye", m_cancel)
+			.ContinueWith(t => m_socket = null);
+	}
 }
---
+++ Assets/TestUI.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Net.WebSockets;
+
+public class TestUI : MonoBehaviour
+{
+	[SerializeField]
+	private Test m_test;
+
+	[SerializeField]
+	private Toggle m_tls;
+
+	[SerializeField]
+	private Button m_connect;
+
+	[SerializeField]
+	private Button m_send;
+
+	[SerializeField]
+	private Button m_disconnect;
+
+	[SerializeField]
+	private Text m_sendText;
+
+	[SerializeField]
+	private Text m_recvText;
+
+
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		m_tls.SetIsOnWithoutNotify(m_test.IsUsingTls);
+		m_tls.onValueChanged.AddListener(b => m_test.IsUsingTls = b);
+
+		// Connect
+		m_connect.interactable = true;
+		m_connect.onClick.AddListener(delegate ()
+		{
+			var state = m_test.WebSocketState;
+			bool closed = state == WebSocketState.Closed || state == WebSocketState.None;
+			if (!closed)
+				return;
+			Debug.Log("UI requesting to connect.");
+			m_test.StartConnecting();
+		});
+
+
+		// Send
+		m_send.interactable = false;
+		m_send.onClick.AddListener(delegate ()
+		{
+			if (!m_test.IsReadyToSend)
+				return;
+			Debug.Log("UI requesting to send.");
+			m_test.DoWebsocketSend();
+		});
+
+		// Disconnect
+		m_disconnect.interactable = false;
+		m_disconnect.onClick.AddListener(delegate ()
+		{
+			Debug.Log("UI requests disconnection.");
+			m_test.StartDisconnecting();
+		});
+
+		m_test.MessageSent += (s) => m_sendText.text = $"Sent: {s}";
+		m_test.MessageReceived += (s) => m_recvText.text = $"Recv: {s}";
+
+
+		m_test.WebSocketStateChanged += delegate ()
+		{
+			var s = m_test.WebSocketState;
+			Debug.Log($"Socket state becomes {s}.");
+			// There are many states, but the logic for the buttons is simple:
+			// The connect button is interactable only when disconnected.
+			// The other buttons are interactable only when connected.
+			// In any other state, no buttons are interactable.
+			bool closed = s == WebSocketState.Closed || s == WebSocketState.None;
+			bool connected = s == WebSocketState.Open;
+			m_connect.interactable = closed;
+			m_tls.interactable = closed;
+			m_send.interactable = connected;
+			m_disconnect.interactable = connected;
+
+			var es = EventSystem.current;
+			Button newSelection = closed ? m_connect : connected ? m_send : null;
+			if (newSelection != null)
+				es.SetSelectedGameObject(newSelection.gameObject);
+		};
+	}
+
+
+}
